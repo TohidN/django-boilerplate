089aed5a2a9ac13a746e4fc3c52526e1a54c8657
Towhid <Towhidn@users.noreply.github.com>
2017-10-14 19:37:31 -0800

    remove admin if it already exists

--- project/management/commands/create-admin.py
+++ project/management/commands/create-admin.py
@@ -6,7 +6,15 @@ class Command(BaseCommand):
 	help = 'create admin'
 
 	def handle(self, *args, **options):
+		try:
+			user = User.objects.get(username='admin')
+			user.delete()
+			print("Admin already existed. it's removed now.")
+		except User.DoesNotExist:
+			pass
+
 		user = User.objects.create_user('admin', password='admin')
 		user.is_superuser = True
 		user.is_staff = True
 		user.save()
+		print("Admin is created.")
